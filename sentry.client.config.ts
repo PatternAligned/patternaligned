d1e5bc6973c7e52b7b4511b3cf51a1f3d508509a
PatternAligned <leftymediaco@gmail.com>
2026-03-11 13:26:36 -0500

    Fix Sentry Replay API for v8

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,7 +3,7 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   integrations: [
-    new Sentry.Replay({
+    Sentry.replayIntegration({
       maskAllText: true,
       blockAllMedia: true,
     }),
